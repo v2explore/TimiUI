ba4bdcb13bfcdcc48b61e72414d8577d3f8bf630
v2explore <v2explore@foxmail.com>
2021-03-10 00:19:39 +0800

    fix: 完善icon单元测试

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Icon from './icon';
+import Icon from './icon/icon';
 
 const fn: React.MouseEventHandler = (e) => {
   console.log('icon', e.target)
 }
 
-ReactDOM.render(<div><Icon name="bin" onClick={fn} /></div>, document.querySelector('#root'))
+ReactDOM.render(<div><Icon name="wechat" onClick={fn} /></div>, document.querySelector('#root'))
