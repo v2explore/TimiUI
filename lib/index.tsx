4a1a3025cf4d6b7c977dc7796b85e70932fe5065
v2explore <v2explore@foxmail.com>
2021-03-11 23:57:42 +0800

    fix: 完善ci配置

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 import Icon from './icon/icon';
 
-const fn: React.MouseEventHandler = (e) => {
-  console.log('icon', e.target)
+export {
+  Icon
 }
-
-ReactDOM.render(<div><Icon name="wechat" onClick={fn} /></div>, document.querySelector('#root'))
+// ReactDOM.render(<div><Icon name="wechat" /></div>, document.querySelector('#root'))
