4218736184ff4bf00e5ed50890c2d087c408e0c5
v2explore <v2explore@foxmail.com>
2021-03-16 23:31:11 +0800

    fix: 完成diaLog

--- examples/dialog.example.tsx
+++ examples/dialog.example.tsx
@@ -1,25 +1,60 @@
 import React, { useState } from 'react';
-import Dialog, { alert } from '../lib/dialog/dialog';
+import Dialog, { alert, confirm } from '../lib/dialog/dialog';
 
 export default function () {
   const [visible, setVisible] = useState(false);
-  const [x, setx] = useState(false);
+  const [visible2, setVisible2] = useState(false);
+  const [text, setText] = useState('Click me!');
   return (
     <div>
       <h2>Dialog</h2>
       <div>
         <button onClick={() => setVisible(!visible)}>Button</button>
-        <Dialog visible={visible} onClose={() => setVisible(false)}>
-          弹框内容
+        <Dialog
+          visible={visible}
+          onClose={() => setVisible(false)}
+          footer={
+            <button
+              style={{ width: 100 }}
+              onClick={() => {
+                setText(text === '😎' ? '😊' : '😎');
+              }}
+            >
+              {text}
+            </button>
+          }
+        >
+          Here is some content...
         </Dialog>
       </div>
       <div>
-        <button onClick={() => setx(!visible)}>Button</button>
-        <Dialog visible={x} onClose={() => setx(false)}>
-          弹框内容
+        <button onClick={() => setVisible2(!visible)}>Button</button>
+        <Dialog visible={visible2} onClose={() => setVisible2(false)}>
+          Here is some content...
         </Dialog>
       </div>
-      <button onClick={() => alert('234')}>alert</button>
+      <div>
+        <button
+          onClick={() =>
+            alert('Here is some content...', () => console.log('ok'))
+          }
+        >
+          alert
+        </button>
+      </div>
+      <div>
+        <button
+          onClick={() =>
+            confirm(
+              'Here is some content...',
+              () => console.log('yes'),
+              () => console.log('no'),
+            )
+          }
+        >
+          confirm
+        </button>
+      </div>
     </div>
   );
 }
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,27 +1,37 @@
-import React from 'react';
+import React, { ReactElement, ReactFragment, ReactNode } from 'react';
 import ReactDOM from 'react-dom';
 import { scopedClassMaker } from '../utils/classes';
 import { Icon } from '../index';
 import './dialog.scss';
 
 interface Props {
+  title?: string,
   visible: boolean;
   onClose: React.MouseEventHandler;
   maskClosable?: boolean;
   style?: React.CSSProperties;
+  footer?: ReactFragment | ReactElement | null;
 }
 
 const scopedClass = scopedClassMaker('tm-dialog');
 
 const Dialog: React.FC<Props> = ({
+  title,
   visible,
   children,
   onClose,
   maskClosable,
   style,
+  footer,
 }) => {
   const onClickClose: React.MouseEventHandler = (e) => {
-    onClose(e);
+    if (onClose) {
+      onClose(e);
+    } else {
+      ReactDOM.render(<React.Fragment></React.Fragment>, container);
+      ReactDOM.unmountComponentAtNode(container);
+      container.remove();
+    }
   };
   const onClickMask: React.MouseEventHandler = (e) => {
     if (maskClosable) {
@@ -29,65 +39,86 @@ const Dialog: React.FC<Props> = ({
     }
   };
   const content = visible ? (
-    <>
+    <React.Fragment>
       <div className={scopedClass('mask')} onClick={onClickMask}></div>
       <div className={scopedClass('wrap')} style={style}>
         <div className={scopedClass('close')} onClick={onClickClose}>
           <Icon name="close" color="#fff" />
         </div>
-        <header className={scopedClass('header')}>提示</header>
+        <header className={scopedClass('header')}>{ title || 'Title' }</header>
         <main className={scopedClass('main')}>{children}</main>
-        <footer className={scopedClass('footer')}>
-          <button className={scopedClass('button')}>取消</button>
-          <button className={scopedClass('button')}>确定</button>
-        </footer>
+
+        {footer ? (
+          <footer className={scopedClass('footer')}>{footer}</footer>
+        ) : footer === null ? null : (
+          <footer className={scopedClass('footer')}>
+            <button className={scopedClass('button')} onClick={onClickClose}>
+              取消
+            </button>
+            <button className={scopedClass('button')} onClick={onClickClose}>
+              确定
+            </button>
+          </footer>
+        )}
       </div>
-    </>
+    </React.Fragment>
   ) : null;
-  return ReactDOM.createPortal(content, document.body);
+  const container = document.createElement('div');
+  document.body.appendChild(container);
+  return ReactDOM.createPortal(content, container);
 };
 
 Dialog.defaultProps = {
   maskClosable: true,
 };
 
-export const alert = (content: string) => {
-  const div = document.createElement('div');
-  const component = (
-    <Dialog
-      visible
-      onClose={() => {
-        ReactDOM.render(<></>, div);
-        // ReactDOM.render(React.cloneElement(component, { visible: false }), div);
-        ReactDOM.unmountComponentAtNode(div);
-        div.remove();
-      }}
-    >
-      {content}
-    </Dialog>
-  );
-  document.body.append(div);
-  ReactDOM.render(component, div);
+const createModal = (
+  content: ReactNode,
+  footer?: ReactFragment | ReactElement | null,
+) => {
+  const render = (props: Props, children: ReactNode) => {
+    ReactDOM.render(React.createElement(Dialog, props, children), container);
+  };
+  const onClose = () => {
+    render({ ...props, visible: false }, content);
+    ReactDOM.unmountComponentAtNode(container);
+    container.remove();
+    return true;
+  };
+  const props = {
+    visible: true,
+    onClose,
+    footer,
+  };
+  const container = document.createElement('div');
+  document.body.appendChild(container);
+  render(props, content);
+  return onClose;
 };
 
-export const confirm = (content: string) => {
-  const div = document.createElement('div');
-  const component = (
-    <Dialog
-      visible
-      onClose={() => {
-        ReactDOM.render(<></>, div);
-        // ReactDOM.render(React.cloneElement(component, { visible: false }), div);
-        ReactDOM.unmountComponentAtNode(div);
-        div.remove();
-      }}
-    >
-      {content}
-    </Dialog>
+export const alert = (content: ReactNode, callback: () => void) => {
+  const close = createModal(
+    content,
+    <button onClick={() => close() && callback && callback()}>确定</button>,
   );
-  document.body.append(div);
-  ReactDOM.render(component, div);
 };
 
+export const confirm = (
+  content: ReactNode,
+  yes: () => void,
+  no?: () => void,
+) => {
+  const close = createModal(
+    content,
+    <React.Fragment>
+      <button className={scopedClass('button')} onClick={() => close() && no && no()}>取消</button>
+      <button className={scopedClass('button')} onClick={() => close() && yes && yes()}>确定</button>
+    </React.Fragment>,
+  );
+};
+
+export const modal = (content: ReactNode) => {
+  return createModal(content);
+};
 
 export default Dialog;
