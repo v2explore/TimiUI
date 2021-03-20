b7dc11fa3768dc7d79d5497d248193dcfdb7c429
v2explore <v2explore@foxmail.com>
2021-03-20 17:48:38 +0800

    update

--- webpack.config.doc.js
+++ webpack.config.doc.js
@@ -14,7 +14,7 @@ module.exports = Object.assign({}, base, {
     new HtmlWebpackPlugin({
       title: 'TiMi UI',
       template: 'example.html',
-      filename: 'example.html'
+      filename: 'index.html'
     }),
   ],
 });
